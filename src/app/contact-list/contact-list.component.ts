76940402fb3fb92f14a9b46f9ba5b54c1020f3c0
Chien Hoang <hoangducchien97@gmail.com>
2019-07-21 21:48:22 +0700

    ngIf, ngFor, ngSwitch, ... build-in Directives

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -3,18 +3,41 @@ import {Component} from '@angular/core';
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'tp-contact-list',
-  template: `
-    <h2>{{message}}</h2>
-    <button (click) = "doClick()">Click me</button>
-  `
+  templateUrl : './contact-list.component.html'
 })
 
 export class ContactListComponent {
-  message = 'Xin chao, ContactListComponent';
-  doClick(){
-    this.message = 'Button clicked';
-    setTimeout(()=>{
-      this.message = 'Button Done';
-    }, 1000);
+  message: string = 'Xin chao, ContactListComponent';
+  isRender = true;
+  tabIndex: number = 1;
+  contacts = [
+    {
+      id: 1,
+      name: 'Chien',
+      job: 'Develop',
+      avatar: {
+        url : 'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+      }
+    },
+    {
+      id: 2,
+      name: 'Dung',
+      job: 'Studen',
+      // avatar: {
+        // url : 'https://iso.500px.com/wp-content/uploads/2016/02/stock-photo-133673159-1500x1000.jpg'
+      // }
+    },
+    {
+      id: 3,
+      name: 'Manh',
+      job: 'Banker',
+      avatar: {
+        url : 'https://www.free-largeimages.com/wp-content/uploads/2015/07/Desktop_Wallpaper_01.jpg'
+      }
+    }
+  ]
+
+  changeTab(tabIndex){
+    this.tabIndex = tabIndex;
   }
 }
